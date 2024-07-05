801a45b2d698bfaf4bdef92af347b456f78575ac
Jo Youngjae <joyoungjae0401@gmail.com>
2024-07-05 12:25:46 +0900

    feat: add log model file

---
+++ flask_app/src/models/log.py
@@ -0,0 +1,35 @@
+from database import Database
+from datetime import datetime
+
+'''
+CREATE TABLE `logs` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `level` varchar(10) DEFAULT NULL,
+    `message` longtext,
+    `created_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+)
+'''
+
+class Log:
+    def save(level, message):
+        db = Database()
+        connection = db.get_connection()
+        cursor = connection.cursor()
+
+        try:
+            insert_query = '''
+            INSERT INTO logs (level, message, created_at)
+            VALUES (%s, %s, %s)
+            '''
+            cursor.execute(insert_query, (
+                level,
+                message,
+                datetime.now()
+            ))
+            connection.commit()
+        except Exception as e:
+            raise
+        finally:
+            cursor.close()
+            
