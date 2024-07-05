10b3c3cd9e31f4b41c47506d22f46582713ef8d8
Jo Youngjae <joyoungjae0401@gmail.com>
2024-07-05 12:26:37 +0900

    feat: add api_log model file

---
+++ flask_app/src/models/api_log.py
@@ -0,0 +1,39 @@
+from database import Database
+from datetime import datetime
+
+'''
+CREATE TABLE `api_logs` (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `api_type` varchar(100) DEFAULT NULL,
+    `product_id` bigint DEFAULT NULL,
+    `cafe24_product_id` bigint DEFAULT NULL,
+    `response` longtext,
+    `created_at` datetime DEFAULT NULL,
+    PRIMARY KEY (`id`)
+)
+'''
+
+class ApiLog:
+    def save(api_type, product_id, cafe24_product_id, response):
+        db = Database()
+        connection = db.get_connection()
+        cursor = connection.cursor()
+
+        try:
+            insert_query = '''
+            INSERT INTO api_logs (api_type, product_id, cafe24_product_id, response, created_at)
+            VALUES (%s, %s, %s, %s, %s)
+            '''
+            cursor.execute(insert_query, (
+                api_type,
+                product_id,
+                cafe24_product_id,
+                response,
+                datetime.now()
+            ))
+            connection.commit()
+        except Exception as e:
+            raise
+        finally:
+            cursor.close()
+            
