268ba5601bc8d2ccca3cabb5276777cb774e8c29
Jo Youngjae <joyoungjae0401@gmail.com>
2024-07-12 17:03:57 +0900

    refactor: fix typo in api log file

--- python_app/src/models/api_log.py
+++ python_app/src/models/api_log.py
@@ -6,7 +6,7 @@ CREATE TABLE `api_logs` (
     `id` bigint NOT NULL AUTO_INCREMENT,
     `type` varchar(50) DEFAULT NULL,
     `message` varchar(100) DEFAULT NULL,
-    `orgin_key` varchar(100) DEFAULT NULL,
+    `origin_key` varchar(100) DEFAULT NULL,
     `cafe24_key` varchar(100) DEFAULT NULL,
     `response` longtext,
     `created_at` datetime DEFAULT NULL,
@@ -15,20 +15,20 @@ CREATE TABLE `api_logs` (
 '''
 
 class ApiLog:
-    def save(type=None, message=None, orgin_key=None, cafe24_key=None, response=None):
+    def save(type=None, message=None, origin_key=None, cafe24_key=None, response=None):
         db = Database()
         connection = db.get_connection()
         cursor = connection.cursor()
 
         try:
             insert_query = '''
-                INSERT INTO api_logs (type, message, orgin_key, cafe24_key, response, created_at)
+                INSERT INTO api_logs (type, message, origin_key, cafe24_key, response, created_at)
                 VALUES (%s, %s, %s, %s, %s, %s)
             '''
             cursor.execute(insert_query, (
                 type,
                 message,
-                orgin_key,
+                origin_key,
                 cafe24_key,
                 response,
                 datetime.now()
